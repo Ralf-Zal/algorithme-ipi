aa9f158b089a7b82434ee5f3dfff9c00fce7a9ee
Ralf <121404991+Ralf-Zal@users.noreply.github.com>
2024-01-22 12:27:01 +0100

    Update 5.py
    Signed-off-by: Ralf <121404991+Ralf-Zal@users.noreply.github.com>

---
+++ 5.py
@@ -0,0 +1,6 @@
+# Calcul d’une facture selon le nombre de photocopies effectuées (calculs par paliers): Voici comment sont facturées les photocopies dans un service de reproduction.
+#Les 10 premières photocopies sont facturées au prix de 0.1 euro par photocopie.
+#Entre 11 et 20 photocopies : les 10 premières sont au prix de 0.1 euros par photocopie, les suivantes sont au prix de 0.08 euro par photocopie.
+#Entre 21 et 50 photocopies : les 10 premières sont au prix de 0.1 euros par photocopie, les 10 suivantes sont au prix de 0.08 euro par photocopie, et les suivantes sont au prix de 0.06 euro par photocopie.
+#A partir de 51 photocopies : les 10 premières sont au prix de 0.1 euros par photocopie, les 10 suivantes sont au prix de 0.08 euro par photocopie, les 30 suivantes sont au prix de 0.06 euro par photocopie, et ensuite chaque photocopie coûte 0.03 euro.
+#Faites saisir un nombre de photocopies et affichez-le. Calculez le montant de la facture correspondante et affichez-le.
