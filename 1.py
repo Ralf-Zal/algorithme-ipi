3c11a63ad06c9dcc13a35355985e12be001bb95f
Ralf <121404991+Ralf-Zal@users.noreply.github.com>
2024-01-22 12:20:33 +0100

    Update 1.py
    Signed-off-by: Ralf <121404991+Ralf-Zal@users.noreply.github.com>

--- 1.py
+++ 1.py
@@ -1,3 +1,6 @@
+#Faîtes saisir une première variable nommée v1, de type entier, et affichez-la. 
+#Faîtes saisir une deuxième variable nommée v2, de type entier, et affichez-la. 
+#Inversez le contenu des deux variables et affichez les deux variables une fois qu’elles sont inversées
 v1 = "v1"
 print(v1)
 v2 = "v2"
