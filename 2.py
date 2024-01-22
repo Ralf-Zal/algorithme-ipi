f2ff06bc89b065dd56b17c0b3e68770ff93a2a49
Ralf <121404991+Ralf-Zal@users.noreply.github.com>
2024-01-22 12:25:27 +0100

    Update 2.py
    Signed-off-by: Ralf <121404991+Ralf-Zal@users.noreply.github.com>

--- 2.py
+++ 2.py
@@ -1,3 +1,7 @@
+# Calculs HT, TVA et TTC: Faîtes saisir un montant hors-taxes de type réel, et affichez-le.
+#Faîtes saisir un taux de TVA de type réel, et affichez-le.
+#Calculez le montant de la TVA de type réel, et affichez-le.
+#Calculez le montant TTC de type réel, et affichez-le.
 ht = float(input("saisir montant HT : "))
 print('le monatant ht est :',ht)
 tva = float(input("saisir taux TVA"))
